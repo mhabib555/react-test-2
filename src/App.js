d3c6b486bb6d94fe9b94aa6af2c7fe9727a8b46d
Muhammad Habib <mhabib555@gmail.com>
2022-10-10 22:33:38 +0300

    Added Light dark theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import {Link, Outlet} from 'react-router-dom';
 import Router from './router';
+import { useState } from 'react';
 
 function App() {
+
+    const [themeMode, setThemeMode] = useState('Dark');
+    const changeTheme = (e)=>{
+        if(e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            setThemeMode('Light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            setThemeMode('Dark');
+        }        
+    }
     return (
         <div className="App">
             <div className="h100">
@@ -13,6 +25,13 @@ function App() {
                     <Link to="/signup">Signup</Link>
                     <Link to="/fetchdata">Fetch Data</Link>
                 </nav>
+                <div>
+                    Dark/light Theme
+                    <label className="slider-btn">
+                        <input type="checkbox" onChange={changeTheme}/>
+                    </label>
+                </div>
+                    
                 <div className="flex flex-column v-align-center">
                     <Router/>
                 </div>
